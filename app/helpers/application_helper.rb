2d73247553788087b0881d767dce5ea8d0f9a5b8
bentonwong <bentonwong@gmail.com>
2017-04-27 23:07:13 -0700

    fix cart bug and update models and helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def current_cart
+    Cart.find_by(id: User.find_by(id: current_user.id).current_cart_id)
+  end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,19 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
+
+  def total
+    LineItem.where("cart_id = ?", id).collect{|li| li.quantity * Item.find_by(id: li.item_id).price}.sum
+  end
+
+  def add_item(item_id)
+    line_item = LineItem.find_by(item_id: item_id, cart_id: self.id)
+    if line_item
+      line_item.tap {|obj| obj.quantity += 1}
+    else
+      LineItem.new(item_id: item_id, cart_id: self.id)
+    end
+  end
 
 end
