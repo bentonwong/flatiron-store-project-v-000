96efb685651eca3ebd0d99f66f37cddc3ef0397d
bentonwong <bentonwong@gmail.com>
2017-05-01 12:31:42 -0700

    fix bug with checkout and adding to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,19 @@
 class CartsController < ApplicationController
 
   def show
+    @cart = Cart.find_by(id: params[:id])
   end
+
+  def checkout
+    LineItem.where("cart_id = ?", params[:id]).each do |line_item|
+      item = Item.find_by(id: line_item.item_id)
+      item.inventory  -= line_item.quantity
+      item.save
+    end
+    user = current_user
+    user.current_cart_id = nil
+    user.save
+    redirect_to cart_path(user.carts.last)
+  end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,7 @@
 class ItemsController < ApplicationController
 
+  def create
+
+  end
+
 end
